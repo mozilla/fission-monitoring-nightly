b9cfe96eeed2757dfd5aa7ac57bb16c1b571b9bf
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2020-09-25 19:21:00 +0200

    Working container

--- test.R
+++ test.R
@@ -1,3 +1,7 @@
-library(bigrquery)
+library(rmarkdown)
 
-print(g$q("select client_id from `moz-fx-data-shared-prod`.telemetry.main where date(submission_timestamp)='2020-01-01' limit 10"))
+setwd('/app')
+
+#print(g$q("select client_id from `moz-fx-data-shared-prod`.telemetry.main where date(submission_timestamp)='2020-01-01' limit 10"))
+
+render('analysis_etl.Rmd', param=list())
