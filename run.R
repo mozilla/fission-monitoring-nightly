d6dcfac1a19e2683a889920ffabf0a0f435b65aa
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2020-09-29 10:55:45 -0700

    Adding in GCS bucket passing and import.

--- run.R
+++ run.R
@@ -3,3 +3,5 @@ library(rmarkdown)
 setwd('/app')
 
 render('analysis_etl.Rmd', output_dir = "/tmp/output/")
+
+#render('dashboard.Rmd', output_dir = 'tmp/output')
--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-export BUCKET=${BUCKET:-gs://moz-fx-data-bq-data-science-cdowhygelund}
+#export BUCKET=${BUCKET:-gs://moz-fx-data-bq-data-science-cdowhygelund}
 
 Rscript /app/run.R
 
-if [[ -z "${GCS_UPLOAD}" ]]; then   
+if [[ -z "${GCS_BUCKET}" ]]; then   
     ## Complete run if RUNFAST environment is missing
     echo "Not uploading Rmarkdown file"
 else
     ## Quick Check if RUNFAST environment is passed
     echo "Uploading ETL markdown file"
-    gsutil -m  rsync -r -d /tmp/output/   $BUCKET/cdowhygelund/fission_monitoring_dashboard/
+    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/analysis_etl.html   $BUCKET/etl/analysis_etl.html
 fi
