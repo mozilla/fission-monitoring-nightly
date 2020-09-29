3be0c5b4a058c822ff04d76ee87c9b109b50caa1
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2020-09-29 12:23:48 -0700

    Adding in `GCS_BUCKET` arg.

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ if [[ -z "${GCS_BUCKET}" ]]; then
 else
     ## Quick Check if RUNFAST environment is passed
     echo "Uploading ETL markdown file"
-    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/analysis_etl.html   $BUCKET/etl/analysis_etl.html
+    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/analysis_etl.html   $GCS_BUCKET/etl/analysis_etl.html
 fi
