167e4b7cdf8343a85dd869b390b177731df946a9
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2020-10-02 19:24:35 +0200

    Fix gsutil call

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ if [[ -z "${GCS_BUCKET}" ]]; then
     echo "Not uploading Rmarkdown file"
 else
     echo "Uploading ETL markdown file"
-    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/analysis_etl.html   $GCS_BUCKET/etl/analysis_etl.html
-    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/dashboard.html   $GCS_BUCKET/dashboard/dashboard.html
+    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/analysis_etl.html   gs://$GCS_BUCKET/etl/analysis_etl.html
+    gsutil -o Credentials:gs_service_key_file=/app/.credentials cp /tmp/output/dashboard.html   gs://$GCS_BUCKET/dashboard/dashboard.html
 fi
