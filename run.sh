eb458a632ded2c35410651b131a7ac79e6e98acd
Corey Dow-Hygelund <godelstheory@gmail.com>
2020-11-06 16:05:53 -0800

    Splitting out `run.sh` into two separate runs, to ensure uploading of analysis markdown to GCS. Removing `try` on deletion of old records. (#18)
    Co-authored-by: Corey Dow-Hygelund <cdowhygelund@mozilla.com>

--- run.sh
+++ run.sh
@@ -4,9 +4,17 @@ set -ex
 Rscript /app/run.R
 
 if [[ -z "${GCS_BUCKET}" ]]; then   
-    echo "Not uploading Rmarkdown file"
+    echo "Not uploading analysis Rmarkdown file"
 else
-    echo "Uploading ETL markdown file"
+    echo "Uploading Analysis ETL markdown file"
     gsutil cp /tmp/output/analysis_etl.html gs://$GCS_BUCKET/etl/analysis_etl.html
-    gsutil cp /tmp/output/dashboard.html gs://$GCS_BUCKET/dashboard/dashboard.html
+fi
+
+Rscript /app/run_dashboard.R
+
+if [[ -z "${GCS_BUCKET}" ]]; then
+  echo "Not dashboard file"
+else
+   echo "Uploading dashboard file"
+   gsutil cp /tmp/output/dashboard.html gs://$GCS_BUCKET/dashboard/dashboard.html
 fi
